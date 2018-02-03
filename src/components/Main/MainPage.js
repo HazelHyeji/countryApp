f02b55faa626934c413c73d77764e465615c1874
HAZEL <hyezico@outlook.com>
2018-02-02 19:03:55 -0500

    add router

--- src/components/Main/MainPage.js
+++ src/components/Main/MainPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Nav from './Main/Buttons';
 import Main from './Main/Form';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 
 class MainPage extends React.Component {
     render() {
