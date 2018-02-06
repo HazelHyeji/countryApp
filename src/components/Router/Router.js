5e60d0f007f01a0e9c9368c72d0436b7367ccfd3
HAZEL <hyezico@outlook.com>
2018-02-06 15:39:25 -0500

    add historyFallback

--- src/components/Router/Router.js
+++ src/components/Router/Router.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 
 
-import SearchCountry from './SearchCountry/SearchCountry';
-import Buttons from './Buttons/Buttons';
-import AllCountries from './AllCountries/AllCountries';
-import Quiz from './Quiz/Quiz';
-import NotFound from './NotFound/NotFound';
+import SearchCountry from '../SearchCountry/SearchCountry';
+import Buttons from '../Buttons/Buttons';
+import AllCountries from '../AllCountries/AllCountries';
+import Quiz from '../Quiz/Quiz';
+import NotFound from '../NotFound/NotFound';
 import CountryApp from '../../components/CountryApp';
 
 const Router = () => (
