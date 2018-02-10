6d52af5c82cab1fb3cad13bc0ea551f236644d43
HAZEL <hyezico@outlook.com>
2018-02-10 00:26:56 -0500

    Add lint and fix bug (do yarn install)

--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Buttons = (props) => (
-        <div>
-            <button value="quiz">Quiz</button>
+const Buttons = props => (
+  <div>
+          <button value="quiz">Quiz</button>
         </div>
-    );
+);
 
 export default Buttons;
